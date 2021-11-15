5d36eed67ff8b9c3816804bab5cd4869460dbf5f
Elabajaba <Elabajaba@users.noreply.github.com>
2021-11-15 17:43:21 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    pollster::block_on(run());
+}
+
+async fn run() {
+    let adapter = wgpu::Instance::new(wgpu::Backends::DX12)
+        .request_adapter(&wgpu::RequestAdapterOptions::default())
+        .await
+        .unwrap();
+
+    println!("{:?}", adapter.get_info());
+    println!("{:?}", adapter.features());
+
+    let (_device, _queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::TEXTURE_ADAPTER_SPECIFIC_FORMAT_FEATURES,
+                // features: wgpu::Features::empty(),
+                ..Default::default()
+            },
+            None,
+        )
+        .await
+        .unwrap();
+}
